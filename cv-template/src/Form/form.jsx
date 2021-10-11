092dc0b49bf1116e641dfb64cf7ac984053bf56d
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-10-10 23:08:53 -0300

    instalación de resume-schema

---
+++ cv-template/src/Form/form.jsx
@@ -0,0 +1,20 @@
+import {cv} from '../resume/resume';
+
+const resume = require('resume-schema');
+
+resume.validate (
+    cv,
+  function (err, report) {
+    if (err) {
+      console.error("The resume was invalid:", err);
+      return;
+    }
+    console.log("Resume validated successfully:", report);
+  },
+  function (err) {
+    console.error("The resume was invalid:", err);
+  }
+);
+
+
+
