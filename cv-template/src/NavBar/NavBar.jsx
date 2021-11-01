0d61ee4b12ab116466d75fd62bae35836f737d89
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-10-31 23:29:44 -0300

    Se agregan links a Template y Form

--- cv-template/src/NavBar/NavBar.jsx
+++ cv-template/src/NavBar/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from '../Button/button.jsx';
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
     return(
         <>
+           <Link to= '/template'><Button text='Template'/></Link>
+           <Link to= '/form'><Button text='Formulario'/></Link>
           <h2 className='navBar'>Completá los campos de tu CV</h2>
         </>
     )
