92f2769ed2769579b59d886cb1d2c35dd349b14d
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-11-09 23:30:27 -0300

    Se arregla estructura del proyecto. Se borrar node_modules

--- src/Template/template.jsx
+++ src/Template/template.jsx
@@ -2,6 +2,7 @@ import React , {useContext} from 'react';
 import { TemplateState } from '../Context/CVContext';
 
 
+
 const Template = () => {
 
       const [state, setState] = useContext(TemplateState);
@@ -15,13 +16,14 @@ const Template = () => {
       return (
           
         <>
-           <div>
-              <p>{state[0]}</p>
-              <p>{state[1]}</p>
-              <p>{state[2]}</p>
-              <p>{state[3]}</p>
-           </div>
-        </>
+          
+            <div>
+                  <p>{state[0]}</p>
+                  <p>{state[1]}</p>
+                  <p>{state[2]}</p>
+                  <p>{state[3]}</p>
+            </div>
+           </>
 
       )
 
