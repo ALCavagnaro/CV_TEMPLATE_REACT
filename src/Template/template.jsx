a3ca4e9122c668c95eb737436a94bf38c8c9b4c6
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-11-17 08:27:58 -0300

    Se agrega librería para imprimir PDF

--- src/Template/template.jsx
+++ src/Template/template.jsx
@@ -1,5 +1,7 @@
 import React , {useContext} from 'react';
 import { TemplateState } from '../Context/CVContext';
+import { jsPDF } from "jspdf";
+import Button from '../Button/button';
 
 
 
@@ -9,21 +11,27 @@ const Template = () => {
 
                   
             console.log(state[0]);
-      
 
+      const print = () => {
+            // Default export is a4 paper, portrait, using millimeters for units
+            const doc = new jsPDF();
+
+            doc.text(state, 10, 10);
+            doc.save("a4.pdf");
+      }
      
 
       return (
           
-        <>
-          
+        <> 
             <div>
                   <p>{state[0]}</p>
                   <p>{state[1]}</p>
                   <p>{state[2]}</p>
                   <p>{state[3]}</p>
             </div>
-           </>
+            <Button text='descargar' onClick = {print}/>
+      </>
 
       )
 
