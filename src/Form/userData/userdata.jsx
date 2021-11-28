73486f6a43d841f78f70e378158ccfc129f1c9f4
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-11-28 20:20:28 -0300

    Se acorta el código para generar el formulario

--- src/Form/userData/userdata.jsx
+++ src/Form/userData/userdata.jsx
@@ -1,25 +1,34 @@
 import {cv} from '../../resume/resume';
+import { useContext } from 'react';
+import { TemplateState } from '../../Context/CVContext';
 
 const UserData = () => {
 
-    let userData = [];
+  const [state, setState] = useContext(TemplateState) 
 
-for (let element of Object.keys(cv.basics)) {
-  
-  userData.push(
-    <div className='fields' id={element} key={element}>
-      <label className ='basics'id={element} key={element}>{element}
-        <input/>
-      </label>
-    </div>
-  )
-  
+  const handleInput = (e) => {
+     console.log(e.target.value);
+     setState([...state,e.target.value]);
+  }
+
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
 }
 
 return (
     <>
-    {userData}
-
+    <form className='cvForm' onSubmit={handleSubmit}>
+      {Object.keys(cv.basics).map (element => { 
+        return (
+          <label className='basics' id={element} key={element}>{element}
+              <input type='text' onChange={handleInput}/>
+          </label>
+        )
+      })}
+      <input type='submit' value='Guardar en template'/>
+    </form>
     </>
 )
 
