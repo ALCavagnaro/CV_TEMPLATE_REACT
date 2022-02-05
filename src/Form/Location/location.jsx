aa38b924ddf043ac6b48d3953974234572ba10f0
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-02-05 17:26:55 -0300

    Se agrega localidad y cambio de formato encabezado PDF

--- src/Form/Location/location.jsx
+++ src/Form/Location/location.jsx
@@ -1,26 +1,51 @@
 import {cv} from '../../resume/resume';
+import { useContext, useState } from 'react';
+import { TemplateState } from '../../Context/CVContext';
 
 const Location = () => {
 
-let location = []
+ const [state, setState] = useContext(TemplateState);
 
-for (let element of Object.keys(cv.basics.location)) {
+const handleSubmit = (e) =>{
 
-  location.push(
-    <div className='fields' id={element} key={element}>
-      <label className ='basics'id={element} key={element}>{element}
-        <input/>
-      </label>
-    </div>
-  )
+ e.preventDefault();
+
+ let location = []
+
+ location.push(e.target[0].value);                     //TODO: pasar de array a un OBJ 
+ location.push(e.target[1].value);
+
+setState([...state, location]);              //Se guarada una imagen arriba de todo. Arreglar eso
+console.log(state);
 
 }
+
+const eraseContent = (e) => {
+  e.preventDefault();
+  setState([state.lenght = 0]);  //Está borrando todo el state!!!
+}
+
 
 return (
 
     <>
 
-    {location}
+    <form className='cvForm' onSubmit={handleSubmit}>
+
+    {Object.keys(cv.basics.location).map (element =>
+     
+     <div className='fields' id={element} key={element}>
+      <label className ='basics'id={element} key={element}>{element}
+        <input/>
+      </label>
+    </div>
+
+    ) }
+    
+      <input type='submit' value='Guardar en Template'/>
+      <input type='reset' value='Eliminar' onClick={eraseContent}/>
+
+    </form>
 
     </>
 )
