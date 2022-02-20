03d0256a7543ce844a8471bc3406c9430ec8397e
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-02-20 14:52:42 -0300

    arreglo indentación y saco código duplicado sin usar

--- src/Form/userData/userdata.jsx
+++ src/Form/userData/userdata.jsx
@@ -13,19 +13,13 @@ const UserData = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
 const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    //let userData = []
-
-    let blob = new Blob([selectedImage], { type: "image" });
-// The full Blob Object can be seen 
-// in the Console of the Browser
-//    console.log('Blob - ', blob);
-    let reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = function () {
-    let base64String = reader.result;
-    //setState({id:'img', value:base64String});   
+            
+            e.preventDefault();
+            let blob = new Blob([selectedImage], { type: "image" });
+            let reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = function () {
+            let base64String = reader.result;
   
 
             const form = document.querySelector('#userForm')  // CAMBIAR ESTE CÓDIGO //
@@ -36,27 +30,29 @@ const handleSubmit = (e) => {
 
             for (let item of form) {
             
-              list = new Object();
+                list = new Object();  //Creo un nuevo objeto para guardar los ítems del form en State//
 
-              if (item.id == 'eliminar' || item.id == 'guardar') 
-              {
-              list.id = item.id;
-              list.value = ''}
+                if (item.id === 'eliminar' || item.id === 'guardar') 
+                    {
+                    list.id = item.id;
+                    list.value = ''}
 
-              else if (item.id == 'foto') 
-              {
-                list.id = item.id
-                list.img = base64String
-                returnList.push(list)
-              }
-              
-              else {
-              list.id = item.id;
-              list.value = item.value;
-              returnList.push(list)
-              }
+                else if (item.id === 'foto') 
+                    {
+                      list.id = item.id
+                      list.img = base64String
+                      returnList.push(list)
+                    }
+                    
+                else 
+                    {
+                    list.id = item.id;
+                    list.value = item.value;
+                    returnList.push(list)
+                    }
               
             }
+
             setState(state.lenght = 0);
             setState([returnList]); 
   }
@@ -64,111 +60,97 @@ const handleSubmit = (e) => {
     
 }
 
-// const eraseContent = (e) => {
-//   e.preventDefault();
-//   setState([state.length = 0]);
-//   setState([...state, <h2 id='pdf-template'>Template vacío</h2>])
-//   console.log(state);
-// }
 
-const loadImg = (e) => {
-//  console.log(e.target.files[0]);
-  setSelectedImage(e.target.files[0]);
-}
-
-    let blob = new Blob([selectedImage], { type: "image" });
-// The full Blob Object can be seen 
-// in the Console of the Browser
-//    console.log('Blob - ', blob);
-    let reader = new FileReader();
-    reader.readAsDataURL(blob);
-    reader.onloadend = function () {
-    let base64String = reader.result;
-//    console.log(base64String)
-}
-
-return (   
-
-    <form id = 'userForm' className='cvForm' onSubmit={handleSubmit}>
-
-    <div className='category-container'>
-
-      <h3 className='title'>1. Información personal</h3>
-
-
-      {Object.entries(cv.basics).map (element => {
-
-        if (element[0] !=='foto' && element[0] !== 'descripción' && element[0] !== 'email') {
-        return (
-          <label className='sub-label' id={element[0]} key={element[0]}>{element[0]}   
-              <input type='text' id={element[0]} placeholder={element[1]}/> 
-          </label>
-        ) }
-
-        else if (element[0] ==='descripción') { 
-          return(
-            <textarea className= 'sub-label'id={element[0]} key={element[0]} placeholder={element[1]}></textarea>
-          )
-        }
-
-        else if (element[0] ==='email') { 
-          return(
-            <label className='sub-label' id={element[0]} key={element[0]} for='email'>{element[0]}   
-              <input type='email' id={element[0]} placeholder={element[1]}/> 
-          </label>
-          )
-        }
-
-        else {
-          return (
-            <><label className='sub-label' id={element[0]} key={element[0]}>{element[0]}
-              <input type='file' id={element[0]} onChange={(loadImg)} />
-            </label><div>
-                {selectedImage && (
-                  <div id='img-container'>
-                    <img alt="not found" width={"185px"} height={'180px'} src={URL.createObjectURL(selectedImage)} />
-                    <br />
-                    <button onClick={() => setSelectedImage(null)}>Quitar</button>
-                  </div>
-                )}
-                <br />
-              </div></>
-          )
-        }
-        
-      })}
-
-      {Object.entries(cv.basics.location).map (element => 
-          
-          <div className='sub-label' id={element[0]} key={element[0]}>
-            <label className ='sub-label'id={element[0]} key={element[0]}>{element[0]}
-              <input input type='text' id={element[0]} placeholder={element[1]}/>
-            </label>
-          </div>
-
-          ) }
-      
-     </div>
-          
-      <div className='category-container'>
-          <h3 className='title'>2. Experiencia laboral</h3>
-          <Work/>
-          <h3 className='title'>3. Educación</h3>
-          <Education/>
-          <h3 className='title'>4. Recomendaciones</h3>
-          <References/>
-          <h3 className='title'>4. Habilidades</h3>
-          <Skills/>
-          <h3 className='title'>5. Intereses</h3>
-          <Interests/>
-      </div>
-      <div className='form-btn-container'> 
-          <input id = 'guardar' type='submit' value='Guardar en template'/>
-          {/* <input id = 'eliminar' type='reset' value='Eliminar' onClick={eraseContent}/> */}
-      </div>
-    </form>
+            const loadImg = (e) => {
     
-)
+              setSelectedImage(e.target.files[0]);
+
+            }
+
+    
+
+    return (   
+
+        <form id = 'userForm' className='cvForm' onSubmit={handleSubmit}>
+
+            <div className='category-container'>
+
+              <h3 className='title'>1. Información personal</h3>
+
+
+              {Object.entries(cv.basics).map (element => {
+
+                if (element[0] !=='foto' && element[0] !== 'descripción' && element[0] !== 'email') {
+                return (
+                  <label className='sub-label-mandatory' id={element[0]} key={element[0]}>{element[0]}   
+                      <input type='text' id={element[0] + `-input`} placeholder={element[1]} className={'mandatory'}/> 
+                  </label>
+                ) }
+
+                else if (element[0] ==='descripción') { 
+                  return(
+                    <textarea className= 'sub-label'id={element[0]} key={element[0]} placeholder={element[1]}></textarea>
+                  )
+                }
+
+                else if (element[0] ==='email') { 
+                  return(
+                    <label className='sub-label-mandatory' id={element[0]} key={element[0]} for='email'>{element[0]}   
+                      <input type='email' id={element[0] + `-input`} placeholder={element[1]} className={'mandatory'}/> 
+                  </label>
+                  )
+                }
+
+                else {
+                  return (
+                    <><label className='sub-label' id={element[0] + `-label`} key={element[0]}>{element[0]}
+                      <input type='file' id={element[0]} onChange={(loadImg)} />
+                    </label><div>
+                        {selectedImage && (
+                          <div id='img-container'>
+                            <img alt="not found" width={"185px"} height={'180px'} src={URL.createObjectURL(selectedImage)} />
+                            <br />
+                            <button onClick={() => setSelectedImage(null)}>Quitar</button>
+                          </div>
+                        )}
+                        <br />
+                      </div></>
+                  )
+                }
+                
+              })}
+
+              {Object.entries(cv.basics.location).map (element => 
+                  
+                  <div className='sub-label' id={element[0]} key={element[0]}>
+                    <label className ='sub-label'id={element[0]} key={element[0]}>{element[0]}
+                      <input input type='text' id={element[0]} placeholder={element[1]}/>
+                    </label>
+                  </div>
+
+                  ) }
+              
+            </div>
+                  
+              <div className='category-container'>
+                  <h3 className='title'>2. Experiencia laboral</h3>
+                    <Work/>
+                  <h3 className='title'>3. Educación</h3>
+                    <Education/>
+                  <h3 className='title'>4. Recomendaciones</h3>
+                    <References/>
+                  <h3 className='title'>4. Habilidades</h3>
+                    <Skills/>
+                  <h3 className='title'>5. Intereses</h3>
+                    <Interests/>
+              </div>
+              <div className='form-btn-container'> 
+                  <input id = 'guardar' type='submit' value='Guardar en template'/>
+                  {/* <input id = 'eliminar' type='reset' value='Eliminar' onClick={eraseContent}/> */}
+              </div>
+        </form>
+        
+    )
 
 }
 
