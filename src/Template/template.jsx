f970faec4d7646b7f34cf929accc790a01f05cd3
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-02-24 23:58:43 -0300

    clonación de template para imprimir pdf

--- src/Template/template.jsx
+++ src/Template/template.jsx
@@ -10,7 +10,8 @@ const Template = () => {
       const state = context[0];
       console.log('elementos del obj state', state, 'Length:', state.length);
       const stateLength = state.length === 0;
-
+      
+      const text = 'changos';
 
       const eraseContent = (e) => {
               e.preventDefault();
@@ -20,13 +21,23 @@ const Template = () => {
 
       const print = () => {
 
-
             const item = state.map((item) => JSON.stringify(item))
             const doc = new jsPDF();
-            doc.html(document.querySelector("#pdf-template"), {
+            const html = document.querySelector("#pdf-template");
+            const html2 = html.cloneNode(true);
+            html2.style.backgroundColor = 'bisque'; // clonación
+            html2.style.fontSize = '8px';
+            html2.style.width = '50mm';
+            html2.style.height = '72mm';
+            html2.style.margin ='2mm';
+            html2.style.padding = '1mm';
+            
+            doc.html(html2, {
                   callback: function (doc) {
-                    doc.save();
-                  }
+                   console.log(html2);     
+                   doc.save();
+                  },
+
                });
       }
      
@@ -36,26 +47,53 @@ const Template = () => {
             <> 
 
             <button id='btn-descarga' onClick = {print}>Descargar</button>
-            <button id='btn-descarga' onClick = {eraseContent}>Borrar</button>  
+            <button id='btn-borrar' onClick = {eraseContent}>Borrar</button>  
 
               {stateLength ? <h2 id='pdf-template'>Template vacío</h2> : 
               
                   <div id='pdf-template'>
-
-                        <div id= 'user-data-info'>
                         
-                            {state[0].map((item) => (
+                            {state[0].map((item) => {
+                                  
+                                  console.log(item);//item.img para aplicarle tag img
 
-                              item.id === 'foto' ? <img alt='not found' id='pdf-img' src={item.img}></img> : //item.img para aplicarle tag img
+                              if (item.id === 'foto') {
+
+                                    return (
+
+                                          <>
+                                                
+                                                <img alt='not found' id='pdf-img' src={item.img}></img>
+                                          
+                                          </>
+                                    )
+                                    
+                              }
+                                    
+                              else if (item.id === 'nombre-input' || item.id === "puesto-input" || item.id === 'descripción') {
+
+                                    return (
+
+                                          <div id= 'user-data-info' className='user-data-info'>
+                                                <h6 className={item.id}>{item.value}</h6>
+                                          </div>
+
+                                           )        
+                              }
+                               
                               
-                              <p className ='pdf-p' key={item.id}>{item.value}</p>
+                              else {
+                                    
+                                    <p className ='pdf-p' id={item.id} key={item.id}>{item.value}</p>
+                               
+                              }
                               
                               
-                              ))}
+                            })}
 
                           </div>
-                      
-                      </div>}
+                  
+                  }
       
             </>
 
