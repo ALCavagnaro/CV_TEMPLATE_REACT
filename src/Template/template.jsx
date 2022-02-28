617bde8a64a8bc9cbcfd6b8ac67788d0f5cb50ee
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-02-27 23:50:28 -0300

    cambio de tamaño y márgenes de imagen al imprimir pdf

--- src/Template/template.jsx
+++ src/Template/template.jsx
@@ -11,7 +11,7 @@ const Template = () => {
       console.log('elementos del obj state', state, 'Length:', state.length);
       const stateLength = state.length === 0;
       
-      const text = 'changos';
+     // console.log('item.value:', state[0].map((item) => item.value));
 
       const eraseContent = (e) => {
               e.preventDefault();
@@ -23,18 +23,26 @@ const Template = () => {
 
             const item = state.map((item) => JSON.stringify(item))
             const doc = new jsPDF();
+           // const img = document.querySelector('#pdf-img');
+           // const img2 = img.cloneNode(true);
+           // img2.style.width = '3px';
+           // img2.style.border = 'black 5px solid';
+           // img2.style.margin = '0'
             const html = document.querySelector("#pdf-template");
             const html2 = html.cloneNode(true);
             html2.style.backgroundColor = 'bisque'; // clonación
-            html2.style.fontSize = '8px';
+            html2.style.fontSize = '5px';
             html2.style.width = '50mm';
             html2.style.height = '72mm';
             html2.style.margin ='2mm';
             html2.style.padding = '1mm';
-            
+            html2.childNodes[0].style.height = '30px';
+            html2.childNodes[0].style.width = '30px';
+            html2.childNodes[0].style.margin = '3px 21mm';
+            console.log(html2.childNodes.div);
+
             doc.html(html2, {
-                  callback: function (doc) {
-                   console.log(html2);     
+                  callback: function (doc) {     
                    doc.save();
                   },
 
@@ -52,10 +60,9 @@ const Template = () => {
               {stateLength ? <h2 id='pdf-template'>Template vacío</h2> : 
               
                   <div id='pdf-template'>
-                        
+
                             {state[0].map((item) => {
-                                  
-                                  console.log(item);//item.img para aplicarle tag img
+                                  //item.img para aplicarle tag img
 
                               if (item.id === 'foto') {
 
@@ -71,14 +78,25 @@ const Template = () => {
                               }
                                     
                               else if (item.id === 'nombre-input' || item.id === "puesto-input" || item.id === 'descripción') {
-
+                                    
                                     return (
 
-                                          <div id= 'user-data-info' className='user-data-info'>
-                                                <h6 className={item.id}>{item.value}</h6>
+                                          <div className= 'user-data-info'>
+                                                <h6 className={item.id} id={item}>{item.value}</h6>
                                           </div>
 
                                            )        
+                              }
+
+                              else if (item.id==='email-input' || item.id==='teléfono-input'|| item.id === 'linkedin-input' || item.id ==='localidad' || item.id === 'CP') {
+                                    
+                                    return (
+
+                                          <div className = 'user-contact'>
+                                                <div className={item.id} id={item.id}>{item.value}</div>
+                                          </div>
+
+                                          )
                               }
                                
                               
